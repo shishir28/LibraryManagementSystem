7465964bb41492053904b03bcc65399acc83e83c
shishir28 <shishir28@gmail.com>
2018-05-24 00:16:18 +1000

    node sever index.html generated from angular

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,29 @@
-import * as express from 'express'
+
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as path from "path";
+import * as serveStatic from "serve-static";
+
 
 class App {
-  public express
+  public express;
 
-  constructor () {
+  constructor() {
     this.express = express()
     this.mountRoutes()
   }
 
-  private mountRoutes (): void {
-    const router = express.Router()
+  private mountRoutes(): void {
+    const router = express.Router();
+    const root = path.join(path.resolve(__dirname, 'public'));
+
+    // // Static assets
+    this.express.use('/', serveStatic(path.resolve(root)));
+    this.express.use('/favicon.ico', serveStatic(path.resolve(root, 'favicon.ico')));
+
     router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!'
-      })
+      res.sendFile(path.join(root, '/index.html'));
+
     })
     this.express.use('/', router)
   }
